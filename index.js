89b645d2a7cb90e79144a206d85e46a790ea239d
federico <peteco1983@outlook.com>
2019-08-28 10:19:27 +0200

    Add variables to get result

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function rC(clothingColor, lastFoodEaten) {
-  let capital = clothingColor.toUppercase[0];
+  //   let capital = clothingColor.toUppercase;
+  let firstLetter = clothingColor.substring(0, 1);
+  let capital = firstLetter.toUpperCase();
+  let rest = clothingColor.substring(1);
+  let foodFirstletter = lastFoodEaten.substring(0, 1);
+  let foodCapital = foodFirstletter.toUpperCase();
+  let foodRest = lastFoodEaten.substring(1);
+  return `The ${capital + rest} ${foodCapital + foodRest}`;
 }
 
-console.log(rC(brown, beans));
+console.log(rC("brown", "beans"));
